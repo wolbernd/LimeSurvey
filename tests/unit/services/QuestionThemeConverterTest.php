be8f074e689fb878b5cb43609acb92e6dd7f7abe
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-01-21 15:35:26 +0100

    Dev: Add mocks to question theme converter test

--- tests/unit/services/QuestionThemeConverterTest.php
+++ tests/unit/services/QuestionThemeConverterTest.php
@@ -2,19 +2,33 @@
 
 namespace ls\tests;
 
+use LimeSurvey\Models\Services\QuestionThemeConverter;
+use LimeSurvey\Models\Services\XmlIO;
+use Yii;
+
 /**
  * Check the JSON saved in database.
  */
 class QuestionThemeConverterTest extends TestBaseClass
 {
     /**
-     * 
+     * @return void
      */
     public function testBasic()
     {
-        $foo = new \LimeSurvey\Models\Services\QuestionThemeConverter(
-            \Yii::app()->getConfig(),
-            new \LimeSurvey\Models\Services\XmlIO()
+        $converter = new QuestionThemeConverter(
+            ['rootdir' => __DIR__],
+            (new class extends XmlIO {
+                public function save($xml, $path)
+                {
+                    return true;
+                }
+                public function load($path)
+                {
+                    return new \SimpleXMLElement('<test></test>');
+                }
+            })
         );
+        $converter->convert('blablabla');
     }
 }
