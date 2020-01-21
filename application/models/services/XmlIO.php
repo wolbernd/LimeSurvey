914b3b99228eb900f2df66682e0597e112e6cd24
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-01-21 12:06:15 +0100

    Dev: Factor out XmlIO load/save from service

---
+++ application/models/services/XmlIO.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * XML input/output.
+ */
+final class XmlIO
+{
+    /**
+     * @param SimpleXMLElement $xml
+     * @param string $filename
+     * @return mixed
+     */
+    public function save(SimpleXMLElement $xml, $filename)
+    {
+        // If the filename isn't specified, this function returns a string on success and FALSE on error.
+        // If the parameter is specified, it returns TRUE if the file was written successfully and FALSE otherwise.
+        return $xml->saveXml($filename);
+    }
+
+    /**
+     * @param string $path
+     * @return SimpleXMLElement
+     * @throws \Exception
+     */
+    public function load($path)
+    {
+        $oldState = libxml_disable_entity_loader(true);
+        $file = file_get_contents($path);
+        if (empty($file)) {
+            throw new \Exception(
+                sprintf(
+                    gT('Found no file at path %s'),
+                    $path
+                )
+            );
+        }
+        $xml = simplexml_load_string($file);
+        libxml_disable_entity_loader($oldState);
+        return $xml;
+    }
+}
