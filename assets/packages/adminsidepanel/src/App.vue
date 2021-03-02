1a31e78eff40f6b3dd965c40d34199a713cd1e2d
Jessica Höck <thedirtypanda001@gmail.com>
2021-03-02 10:12:31 +0000

    Dev: WIP -- Disabled sidebar -->

--- assets/packages/adminsidepanel/src/App.vue
+++ assets/packages/adminsidepanel/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="Application-Side-Menu">
-        <sidebar suvey="survey" landOnTab="landOnSideTab" v-model="isCollapsed" isCollapsed="isCollapsed"></sidebar>
+       <!-- <sidebar suvey="survey" landOnTab="landOnSideTab" v-model="isCollapsed" isCollapsed="isCollapsed"></sidebar> -->
     </div>
 </template>
 <script>
